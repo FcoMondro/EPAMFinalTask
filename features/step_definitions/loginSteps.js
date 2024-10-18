bdb82ceb65c85025f04ebd3751b6d6e0f2aa3833
Francisco Mondragón <fjgm11@hotmail.com>
2024-10-18 13:11:17 -0600

    first working test using cucumber, clear field bug found

--- features/step_definitions/loginSteps.js
+++ features/step_definitions/loginSteps.js
@@ -1,44 +1,49 @@
-const { expect, browser, $ } = require('@wdio/globals')
+require("chromedriver");
+const { Builder, By, Capabilities, Key } = require('selenium-webdriver');
+const assert = require('assert');
+const capabilities = Capabilities.chrome();
+capabilities.set('chromeOptions', { "w3c": false });
+const driver = new Builder().withCapabilities(capabilities).build();
+
+const { Given, When, Then } = require('@cucumber/cucumber');
+//const { expect } = require('chai');
+
 const LoginPage = require('../../src/po/pages/login.page');
 const credentials = require('../../src/tests/testData');
-//const allureReporter = require('@wdio/allure-reporter').default;
 const loginPage = new LoginPage();
-const { Given, When, Then } = require('@cucumber/cucumber');
-//const describe = require('@wdio/mocha-framework');
 
-// describe("Feature: Login page", () =>{
-//     // beforeEach(async () => {
-//     //     await loginPage.open();
-//     //     //await browser.url("https://www.saucedemo.com/");  //siempre reiniciar el browser si no es un e2e
-//     // });
+Given("the user is on the login page", async () => {
+    await driver.get("https://www.saucedemo.com/");
+});
 
-    // it("UC-1 Test Login form with empty credentials", async () =>{
-        Given("the user is on the login page", async () => {
-            await loginPage.open();
-        });
+When("the user types any credentials in login fields", async () => {
+    // driver.findElement(By.xpath('//input[@data-test="username"]')).sendKeys(credentials.CREDENTIALS.INVALID_CREDENTIALS.email);
+    //await loginPage.loginForm.item('username').setValue(credentials.CREDENTIALS.INVALID_CREDENTIALS.email);
+    // await $("//input[@data-test='username']").setValue("");
+    
+    //await driver.findElement(By.xpath('//input[@data-test="password"]')).sendKeys(credentials.CREDENTIALS.INVALID_CREDENTIALS.password);
+    //await loginPage.loginForm.item('password').setValue(credentials.CREDENTIALS.INVALID_CREDENTIALS.password);
+    // await $("//input[@data-test='password']").setValue(""); 
+});
 
-        When("the user types any credentials in login fields", async () => {
-            await loginPage.loginForm.item('username').setValue(credentials.CREDENTIALS.INVALID_CREDENTIALS.email);
-            // await $("//input[@data-test='username']").setValue("");
-            await loginPage.loginForm.item('password').setValue(credentials.CREDENTIALS.INVALID_CREDENTIALS.password);
-            // await $("//input[@data-test='password']").setValue(""); 
-        });
+When("the user clears both fields", async () => {
+    await driver.findElement(By.xpath('//input[@data-test="username"]')).clear();
+    //await loginPage.loginForm.clearField('username');
 
-        When("the user clears both fields", async () => {
-            await loginPage.loginForm.clearField('username');
-            await loginPage.loginForm.clearField('password');
-        });
+    await driver.findElement(By.xpath('//input[@data-test="password"]')).clear();
+    //await loginPage.loginForm.clearField('password');
+});
 
-        When("clicks the 'Login' button", async () => {
-            await loginPage.loginForm.item('logginButton').click();
-            // await $("//input[@data-test='login-button']").click();
-        });
+When("clicks the {string} button", async (string) => {
+    await driver.findElement(By.xpath('//input[@data-test="login-button"]')).click();
+    //await loginPage.loginForm.item('logginButton').click();
+    // await $("//input[@data-test='login-button']").click();
+});
 
-        Then("'Username is required' error is displayed", async () => {
-            await expect(await loginPage.loginForm.item('loginError').getText()).toEqual('Epic sadface: Username is required');
-            // const loginError = await $("//div[@class='error-message-container error']/h3");
-            // await expect(await loginError.getText()).toEqual('Epic sadface: Username is required');
+Then("{string} error is displayed", async (string) => {
+    await assert.equal(await driver.findElement(By.xpath('//h3[@data-test="error"]')).getText(), 'Epic sadface: Username is required');
+    //await expect(await loginPage.loginForm.item('loginError').getText()).toEqual('Epic sadface: Username is required');
+    // const loginError = await $("//div[@class='error-message-container error']/h3");
+    // await expect(await loginError.getText()).toEqual('Epic sadface: Username is required');
 
-        });
-//     });
-// });
+});
--- src/po/components/common/loginForm.component.js
+++ src/po/components/common/loginForm.component.js
@@ -4,32 +4,23 @@ class LoginFormComponent {
     //     return $('//div[@class="login-box"]');
     // }
     
-    /**
-     * 
-     * @param {string} param 
-     * @returns 
-     */
     item (param) {
         const selectors = {
-            username: 'input[@data-test="username"]',
-            password: 'input[@data-test="password"]',
-            logginButton: 'input[@data-test="login-button"]',
-            loginError: 'h3[@data-test="error"]'
+            username: '//input[@data-test="username"]',
+            password: '//input[@data-test="password"]',
+            logginButton: '//input[@data-test="login-button"]',
+            loginError: '//h3[@data-test="error"]'
         };
 
         //return this.rootEL.$(`//${selectors[param]}`);
-        return $(`//${selectors[param]}`);
+        return this.item(param);
     }
 
-    /**
-     * 
-     * @param {string} param 
-     */
     async clearField (param) {
         await this.item(param).click();
         // Simulate Ctrl + A (Select all) and Delete
-        await browser.keys(['Control', 'a']);
-        await browser.keys('Delete');
+        await driver.keys(['Control', 'a']);
+        await driver.keys('Delete');
     }
 }
 

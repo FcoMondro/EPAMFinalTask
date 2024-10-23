ec4d836dc8140bf5417945c5a4f8305708451d4e
Francisco Mondragón <fjgm11@hotmail.com>
2024-10-22 19:28:19 -0600

    first modification of login page object

--- src/po/pages/login.page.js
+++ src/po/pages/login.page.js
@@ -3,13 +3,33 @@ const LoginFormComponent = require('./../components/common/loginForm.component')
 class LoginPage {
 
     constructor() {
-        this.loginForm = new LoginFormComponent();
+        //this.page = page;
+        //this.loginForm = new LoginFormComponent(); //eliminar
+        this.usernameField = $(`//$input[@data-test="username"]`);
+        this.passwordField = $(`//$input[@data-test="password"]`);
+        this.logginButton = $(`input[@data-test="login-button"]`);
+        this.loginErrorBanner = $(`h3[@data-test="error"]`);
     }
 
     
-    async open () {
+    
+    async openLoginPage () {
         await browser.url("https://www.saucedemo.com/");
     }
+
+    async fillLoginFields(user, password) {
+        await this.usernameField.setValue(user);
+        await this.passwordField.setValue(password);
+    }
+
+    async clearField (param) {
+        await this.item(param).click();
+        // Simulate Ctrl + A (Select all) and Delete
+        await browser.keys(['Control', 'a']);
+        await browser.keys('Delete');
+    }
+
+
     
 }
 

a44c54c454042a37a840b9ee4a0d1f601ef71bc6
Francisco Mondragón <fjgm11@hotmail.com>
2024-11-09 14:52:16 -0600

    update

--- src/tests/test.e2e.js
+++ src/tests/test.e2e.js
@@ -1,7 +1,7 @@
 const { expect, browser } = require('@wdio/globals')
 const LoginPage = require('../po/pages/login.page');
 const credentials = require('./testData');
-const allureReporter = require('@wdio/allure-reporter').default;
+const AllureReporter = require('@wdio/allure-reporter').default;
 
 describe("Final Task", () => {
     describe('Swag Labs Login', () => {
@@ -15,49 +15,49 @@ describe("Final Task", () => {
         });
 
         it("UC-1 Test Login form with EMPTY credentials", async () =>{
-            allureReporter.addStep('Type any credentials into "Username" and "Password" fields.');
+            AllureReporter.addStep('Type any credentials into "Username" and "Password" fields.');
             await loginPage.setUsernameField(credentials.INVALID_CREDENTIALS.email);
             await loginPage.setPasswordField(credentials.INVALID_CREDENTIALS.password);
 
-            allureReporter.addStep('Clear the inputs'); 
+            AllureReporter.addStep('Clear the inputs'); 
             await loginPage.clearUsernameField();
             await loginPage.clearPasswordField();
 
-            allureReporter.addStep('Hit the "Login" button');
+            AllureReporter.addStep('Hit the "Login" button');
             await loginPage.clickLoginButton();
 
-            allureReporter.addStep('Check the error messages: "Username is required"');
+            AllureReporter.addStep('Check the error messages: "Username is required"');
             await expect(await loginPage.getErrorText()).toEqual('Epic sadface: Username is required');
         });
 
         it("UC-2 Test Login form with credentials by passing Username", async () =>{
-            allureReporter.addStep('Type any credentials in username');
+            AllureReporter.addStep('Type any credentials in username');
             await loginPage.setUsernameField(credentials.INVALID_CREDENTIALS.email);
             
-            allureReporter.addStep('Enter password'); 
+            AllureReporter.addStep('Enter password'); 
             await loginPage.setPasswordField(credentials.INVALID_CREDENTIALS.password);
             
-            allureReporter.addStep('Clear the "Password" input.'); 
+            AllureReporter.addStep('Clear the "Password" input.'); 
             await loginPage.clearPasswordField();
 
-            allureReporter.addStep('Hit the "Login" button.'); 
+            AllureReporter.addStep('Hit the "Login" button.'); 
             await loginPage.clickLoginButton();
 
-            allureReporter.addStep('Check the error messages: "Password is required".');
+            AllureReporter.addStep('Check the error messages: "Password is required".');
             await expect(await loginPage.getErrorText()).toEqual('Epic sadface: Password is required'); 
         });
 
         it("UC-3 Test Login form with credentials by passing Username & Password", async () => {
-            allureReporter.addStep('Type credentials in username which are under Accepted username are sections.'); 
+            AllureReporter.addStep('Type credentials in username which are under Accepted username are sections.'); 
             await loginPage.setUsernameField(credentials.VALID_CREDENTIALS.email);
             
-            allureReporter.addStep('Enter password as secret sauce.');
+            AllureReporter.addStep('Enter password as secret sauce.');
             await loginPage.setPasswordField(credentials.VALID_CREDENTIALS.password);
             
-            allureReporter.addStep('Click on Login'); 
+            AllureReporter.addStep('Click on Login'); 
             await loginPage.clickLoginButton();
 
-            allureReporter.addStep('Validate the title “Swag Labs” in the dashboard.'); // Validate the title “Swag Labs” in the dashboard.
+            AllureReporter.addStep('Validate the title “Swag Labs” in the dashboard.'); // Validate the title “Swag Labs” in the dashboard.
             await expect(page).toHaveTitle('Swag Labs');
         });
     });

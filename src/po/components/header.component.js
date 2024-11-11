411bb780ce639f8fd29a352db0542dad25fa4850
Francisco Mondragón <fjgm11@hotmail.com>
2024-11-11 17:23:26 -0600

    added functions to both header files

--- src/po/components/header.component.js
+++ src/po/components/header.component.js
@@ -1,10 +1,43 @@
 class HeaderPrimary {
     constructor(page) {
         this.page = page;
-        this.sideMenu = page.$(`//button[@id="react-burger-menu-btn"]`);
+        this.sideMenuButton = page.$(`//button[@id="react-burger-menu-btn"]`);
+        this.closeMenuButton = page.$(`//button[@id="react-burger-cross-btn"]`);
+        this.sideMenu = page.$(`//div[@class="bm-menu-wrap"]`);
         this.logo = page.$(`//div[@class="app_logo"]`);
         this.cartButton = page.$(`//a[@data-test="shopping-cart-link"]`);
-        this.cartBadge = page.$(`//span[data-test="shopping-cart-badge"]`)
+        this.cartBadge = page.$(`//span[data-test="shopping-cart-badge"]`);
+    }
+
+    async openMenu() {
+        let menuButton = await this.sideMenuButton;
+        await menuButton.click();
+    }
+
+    async closeMenu(){
+        let closeMenuButton = await this.closeMenuButton;
+        await closeMenuButton.click();
+    }
+
+    async isMenuVisible(){
+        let sideMenu = await this.sideMenu;
+        if(sideMenu.checkVisibility() == true){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    async getCarItemCount() {
+        let cartBadge = await this.cartBadge;
+        if(cartBadge.isExisting() == false){
+            return 0;
+        }
+        else{
+            return Number(cartBadge.getText());
+        }
+
     }
 
 }
--- src/po/components/headerSecondary.component.js
+++ src/po/components/headerSecondary.component.js
@@ -4,7 +4,16 @@ class HeaderSecondary {
         this.container = page.$(`//div[@data-test="secondary-header"]`);
         this.title = page.$(`//span[@data-test="title"]`);
         this.sortDropdown = page.$(`//select[@data-test="product-sort-container"]`);
+    }
 
+    async getTitle() {
+        let title = await this.title;
+        return title.getText();
+    }
+
+    async orderProductsByValue(value) {
+        let dropDown = await this.sortDropdown;
+        await dropDown.selectByAttribute('value',value);
 
     }
 
